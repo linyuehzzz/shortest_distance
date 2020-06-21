1baa72206fc5e497791b603518ab59e926cd7819
Lin, Yue <lin.3326@osu.edu>
2020-06-20 20:11:25 -0400

    update cuda support

---
+++ scripts/all_pair_distance_cuda.py
@@ -0,0 +1,214 @@
+from timeit import default_timer
+from numba import cuda, njit, float32
+import numpy as np
+import math
+
+
+##******** Read graph data ********##
+
+## Number of nodes (100/1,000/10,000/100,000/1,000,000)
+nodes = [100, 1000, 10000]
+print('Nodes: ', nodes)
+## Total degree
+degree = [3, 4, 5]
+print('Degree: ', degree)
+
+for i in nodes:
+  for j in degree:        
+    locals()['data_n' + str(i) + '_d' + str(j)] = []
+      with open('graph_n' + str(i) + '_d' + str(j) + '.txt', 'r') as f:
+        lines = f.read().splitlines()
+          for line in lines:
+            l = line.split()
+            item = [int(l[0]), int(l[1]), float(l[2])]
+            locals()['data_n' + str(i) + '_d' + str(j)].append(item)
+
+
+
+##******** Configure CUDA ********##
+
+# number of threads per block: 32、128、256
+NUM_THREADS = [4, 16, 32, 64]
+
+def get_cuda_execution_config(n, tpb):
+  dimBlock = (tpb, tpb)
+  dimGrid = (math.ceil(n / tpb), math.ceil(n / tpb))
+
+  return dimGrid, dimBlock
+
+
+
+##******** Construct distance matrix ********##
+
+@cuda.jit
+def graph2dist(graph, dist_mtx, n):
+  stride = cuda.gridDim.x * cuda.blockDim.x
+
+  ## initialize distance matrix
+  x, y = cuda.grid(2)
+  for i in range(x, n, stride):
+    for j in range(y, n, stride):
+      dist_mtx[i,j] = np.inf
+
+  ## calculate distance matrix
+  x = cuda.grid(1)
+  for i in range(x, graph.shape[0], stride):
+    a = int(graph[i,0]) - 1
+    b = int(graph[i,1]) - 1
+    d = graph[i,2]
+    dist_mtx[a,b] = d
+    dist_mtx[b,a] = d
+  
+  ## set diagonal to 0
+  y = cuda.grid(1)
+  if y < n:
+    dist_mtx[y,y] = 0.0
+
+
+def distance_matrix(graph, n):
+  ## copy data to device
+  graph_device = cuda.to_device(graph)
+  dist_mtx_device = cuda.device_array(shape=(n,n))
+
+  ## calculate distance matrix
+  graph2dist[dimGrid, dimBlock](graph_device, dist_mtx_device, n)
+  
+  ## copy data to host
+  dist_mtx_host = dist_mtx_device.copy_to_host()
+ 
+  return dist_mtx_host
+
+
+
+##**** Hedetniemi distance ****##
+
+@cuda.jit
+def init_mtx(matrix, mtx_a_t_1, mtx_a_t, n):
+  # initialize distance matrix
+  x, y = cuda.grid(2)
+  if x < n and y < n:
+    mtx_a_t[x,y] = np.inf
+    mtx_a_t_1[x,y] = matrix[x,y]
+
+
+@cuda.jit
+def all_pair_hedet(matrix, mtx_a_t_1, mtx_a_t, n):
+  x, y = cuda.grid(2)
+  if x < n and y < n:
+    summ = np.inf
+    for k in range(n):
+      summ = min(summ, mtx_a_t_1[x, k] + matrix[k, y])
+    mtx_a_t[x,y] = summ
+
+
+@cuda.jit
+def cmp_mtx(mtx_a_t_1, mtx_a_t, n, p):
+  x, y = cuda.grid(2)
+  if x < n and y < n and p == True:
+    if mtx_a_t_1[x,y] != mtx_a_t[x,y]:
+      p = False
+  
+  if p == False:
+    x, y = cuda.grid(2)
+    if x < n and y < n:
+      mtx_a_t_1[x,y] = mtx_a_t[x,y]
+
+
+def hede_distance(matrix, n):
+  ## copy data to device
+  matrix_device = cuda.to_device(matrix)
+  mtx_a_t_1_device = cuda.device_array(shape=(n,n))
+  mtx_a_t_device = cuda.device_array(shape=(n,n))
+
+  ## initialize hedetniemi distance
+  init_mtx[dimGrid, dimBlock](matrix_device, mtx_a_t_1_device, mtx_a_t_device, n)
+
+  ## calculate hedetniemi distance
+  p = False
+  for k in range(n):
+    all_pair_hedet[dimGrid, dimBlock](matrix_device, mtx_a_t_1_device, mtx_a_t_device, n)
+    cmp_mtx[dimGrid, dimBlock](mtx_a_t_1_device, mtx_a_t_device, n, p)
+    if p == True:
+      break
+  
+  ## copy data to host
+  mtx_a_t_host = mtx_a_t_device.copy_to_host()
+ 
+  return mtx_a_t_host
+
+
+
+##******** Floyd–Warshall distance ********##
+
+@cuda.jit
+def all_pair_floyd(matrix, k, n):
+  x, y = cuda.grid(2)
+  if x < n and y < n:
+    matrix[x,y] = min(matrix[x,y], matrix[x,k] + matrix[k,y])
+
+
+def floyd_distance(matrix, n):
+  ## copy data to device
+  matrix_device = cuda.to_device(matrix)
+
+  ## calculate hedetniemi distance
+  for k in range(n):
+    all_pair_floyd[dimGrid, dimBlock](matrix_device, k, n)
+  
+  ## copy data to host
+  matrix_host = matrix_device.copy_to_host()
+ 
+  return matrix_host
+
+
+
+##******** Main ********##
+
+with open('all_pair_cuda_results.csv', 'w') as fw:
+  fw.write('bpg,tpb,nodes,degree,t1,hedet_t2,floyd_t2,\n')
+  fw.flush()
+
+  for k in NUM_THREADS:
+    for i in nodes:
+      for j in degree:
+        dimGrid, dimBlock = get_cuda_execution_config(i, k)
+
+        data = locals()['data_n' + str(i) + '_d' + str(j)]
+        
+        ## distance matrix
+        try:
+          start = default_timer()
+          dist_mtx = distance_matrix(np.array(data), i)
+          stop = default_timer()
+          cuda_t1 = stop - start
+        except:
+          cuda_t1 = float('inf')
+        
+        ## hedetniemi distance
+        try:
+          start = default_timer()
+          mtx_a_t_hedet = hede_distance(dist_mtx, i)
+          stop = default_timer()
+          cuda_hedet_t2 = stop - start
+          ## print shortest path matrix
+          with open('hedet_dist_nb_cuda' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
+              f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_hedet.tolist()]))
+        except:
+          cuda_hedet_t2 = float('inf')
+            
+        ## floyd distance
+        try:
+          start = default_timer()
+          mtx_a_t_np = floyd_distance_np(dist_mtx_np, i)
+          stop = default_timer()
+          cuda_floyd_t2 = stop - start
+          ## print shortest path matrix
+          with open('floyd_dist_nb_cuda' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
+            f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_floyd.tolist()]))                
+        except:
+          cuda_floyd_t2 = float('inf')       
+            
+        fw.write(str(dimGrid) + ',' + str(dimBlock) + ',' + str(i) + ',' + str(j) + ',' + str(cuda_t1) + ',' + str(cuda_hedet_t2) + ','
+                  + str(cuda_floyd_t2) + '\n')
+
+        fw.flush()
--- scripts/hedetniemi_distance.py
+++ scripts/hedetniemi_distance.py
@@ -10,7 +10,7 @@ from timeit import default_timer
 ##******** Read graph data ********##
 
 ## Number of nodes (100/1,000/10,000/100,000/1,000,000)
-nodes = [100, 1000, 10000, 100000, 1000000]
+nodes = [100, 1000, 10000]
 print('Nodes: ', nodes)
 ## Total degree
 degree = [3, 4, 5]
@@ -170,79 +170,78 @@ def hede_distance_nb(matrix, n):
 ##******** Main ********##
 
 with open('hedet_results.csv', 'w') as fw:
-    fw.write('nodes,degree,list_t1,list_t2,np_t1,np_t2,nb_t1,nb_t2\n')
-
-    fw.flush()
+  fw.write('nodes,degree,list_t1,list_t2,np_t1,np_t2,nb_t1,nb_t2\n')
+  fw.flush()
     
-    for i in nodes:
-        for j in degree:
-            data = locals()['data_n' + str(i) + '_d' + str(j)]
-            
-            ## List t1
-            try:
-                start = default_timer()
-                dist_mtx_list = distance_matrix_list(data, nodes)
-                stop = default_timer()
-                list_t1 = stop - start
-            except:
-                list_t1 = float('inf')
-            
-            ## List t2
-            try:
-                start = default_timer()
-                mtx_a_t_list = hede_distance_list(dist_mtx_list, nodes)
-                stop = default_timer()
-                list_t2 = stop - start
-                ## print shortest path matrix
-                with open('hedet_mtx_list' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
-                    f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_list]))
-            except:
-                list_t2 = float('inf')
-                
-            ## Numpy t1
-            try:
-                start = default_timer()
-                dist_mtx_np = distance_matrix_np(np.array(data), nodes)
-                stop = default_timer()
-                np_t1 = stop - start
-            except:
-                np_t1 = float('inf')
-                
-            ## Numpy t2
-            try:
-                start = default_timer()
-                mtx_a_t_np = hede_distance_np(dist_mtx_np, nodes)
-                stop = default_timer()
-                np_t2 = stop - start
-                ## print shortest path matrix
-                with open('hedet_mtx_np' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
-                    f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_np.tolist()]))                
-            except:
-                np_t2 = float('inf')
-                
-            ## Numba (njit) t1
-            try:
-                start = default_timer()
-                dist_mtx_nb = distance_matrix_nb(np.array(data), nodes)
-                stop = default_timer()
-                nb_t1 = stop - start
-            except:
-                nb_t1 = float('inf')
-            
-            ## Numba (njit) t2
-            try:
-                start = default_timer()
-                mtx_a_t_nb = hede_distance_nb(dist_mtx_nb, nodes)
-                stop = default_timer()
-                nb_t2 = stop - start
-                ## print shortest path matrix
-                with open('hedet_mtx_nb' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
-                    f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_nb.tolist()]))                
-            except:
-                nb_t2 = float('inf')
-                
-            fw.write(str(i) + ',' + str(j) + ',' + str(list_t1) + ',' + str(list_t2) + ','
-                     + str(np_t1) + ',' + str(np_t2) + ',' + str(nb_t1) + ',' + str(nb_t2) + '\n')
+  for i in nodes:
+    for j in degree:
+      data = locals()['data_n' + str(i) + '_d' + str(j)]
+      
+      ## List t1
+      try:
+        start = default_timer()
+        dist_mtx_list = distance_matrix_list(data, i)
+        stop = default_timer()
+        list_t1 = stop - start
+      except:
+        list_t1 = float('inf')
+      
+      ## List t2
+      try:
+        start = default_timer()
+        mtx_a_t_list = floyd_distance_list(dist_mtx_list, i)
+        stop = default_timer()
+        list_t2 = stop - start
+        ## print shortest path matrix
+        with open('floyd_dist_list' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
+            f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_list]))
+      except:
+        list_t2 = float('inf')
+          
+      ## Numpy t1
+      try:
+        start = default_timer()
+        dist_mtx_np = distance_matrix_np(np.array(data), i)
+        stop = default_timer()
+        np_t1 = stop - start
+      except:
+        np_t1 = float('inf')
+          
+      ## Numpy t2
+      try:
+        start = default_timer()
+        mtx_a_t_np = floyd_distance_np(dist_mtx_np, i)
+        stop = default_timer()
+        np_t2 = stop - start
+        ## print shortest path matrix
+        with open('floyd_dist_np' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
+          f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_np.tolist()]))                
+      except:
+        np_t2 = float('inf')
+          
+      ## Numba (njit) t1
+      try:
+        start = default_timer()
+        dist_mtx_nb = distance_matrix_nb(np.array(data), i)
+        stop = default_timer()
+        nb_t1 = stop - start
+      except:
+        nb_t1 = float('inf')
+      
+      ## Numba (njit) t2
+      try:
+        start = default_timer()
+        mtx_a_t_nb = floyd_distance_nb(dist_mtx_nb, i)
+        stop = default_timer()
+        nb_t2 = stop - start
+        ## print shortest path matrix
+        with open('floyd_dist_nb' + '_n' + str(i) + '_d' + str(j) + '.txt', 'w') as f:
+          f.write('\n'.join(['\t'.join([str(round(cell,2)) for cell in row]) for row in mtx_a_t_nb.tolist()]))                
+      except:
+        nb_t2 = float('inf')
+          
+      fw.write(str(i) + ',' + str(j) + ',' + str(list_t1) + ',' + str(list_t2) + ','
+                + str(np_t1) + ',' + str(np_t2) + ',' + str(nb_t1) + ',' + str(nb_t2) + '\n')
 
-            fw.flush()
+      fw.flush()
 fw.close()
